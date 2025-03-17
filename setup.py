8983de68f0d1483aabbab6d5a232dd79f950aebc
Arpan Pal <arpan522000@gmail.com>
2025-03-17 16:53:11 +0530

    0.3.6 - Cubes with header retention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="finalflash",
-    version="0.3.4",
+    version="0.3.6",
     author="Arpan Pal",
     author_email="arpan522000@gmail.com",
     description="A tool for uGMRT primary beam correction",
